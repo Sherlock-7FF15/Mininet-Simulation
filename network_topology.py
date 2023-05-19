f3592caf3f414f9e00855796bbbcf739c9fc320b
Sherlock-Z <58947672+Sherlock-7FF15@users.noreply.github.com>
2023-05-19 14:05:04 -0700

    socket programming

--- network_topology.py
+++ network_topology.py
@@ -3,10 +3,11 @@ from mininet.net import Mininet
 from mininet.node import OVSController
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
+import socket
 
 
 def create_network():
-    "Create a network with 20 hosts and one server."
+    "Create a network with 10 hosts and one server."
 
     net = Mininet(controller=OVSController)
 
@@ -30,9 +31,30 @@ def create_network():
 
     info('*** Running CLI\n')
     while True:
-        command = input('input command')
-
-    CLI(net)
-
+        command = input('Input Command> ')
+        if command == 'start':
+            for node in hosts:
+                # This can be replaced with DDoS attack code
+                node.cmd('ping -c 50 10.0.0.21 &')
+        if command == 'exit':
+            break
+        CLI(net)
     info('*** Stopping network')
     net.stop()
+
+def udp_server(host, port):
+    s_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s_socket.bind((host, port))
+    info('UDP server listening on {}:{}'.format(host, port))
+
+    # Udp Server Listen
+    while True:
+        data, address = s_socket.recvfrom(1024)
+        print('Received data: {}'.format(data.decode()))
+        s_socket.sendto(data, address)
+
+def udp_client(host, port, data):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.sendto(str(data).encode(), (host, port))
+    response, _ = client_socket.recvfrom(1024)
+    print('Received response: {}'.format(response.decode()))
