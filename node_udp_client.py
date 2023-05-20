4f78aa472ec201058a283e28c23c39ac8d6623c4
Sherlock-Z <58947672+Sherlock-7FF15@users.noreply.github.com>
2023-05-19 23:30:25 -0700

    Update node_udp_client.py

--- node_udp_client.py
+++ node_udp_client.py
@@ -1,12 +1,16 @@
 import socket
 import time
-
+import psutil
 
 def client():
     c_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    pre_val = psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv
     while True:
+        new_val = psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv
+        throughput = new_val - pre_val
+        pre_val = new_val
         for i in range(1,11):
             target_ip = '10.0.0.{}'.format(i)
-            c_socket.sendto('10', (target_ip, 9564))
+            c_socket.sendto(str(throughput), (target_ip, 9564))
         time.sleep(1)
 client()
